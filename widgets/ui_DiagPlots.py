fb1d25f07854c0075df6f3601b4f27080f504fba
sblanch <sblanch@a18efa20-c439-0410-bf20-9b91ec695b4c>
2014-07-24 09:16:19 +0000

    attempt to simplify the signalling calculation by the parser module and the gui map and plotting characteristics. This way there a single point of modification in case of changes in those calculation output
    git-svn-id: svn+ssh://controls01/siciliarep/svn/trunk/tango_client/ctrflyrtech_fdl/trunk@12884 a18efa20-c439-0410-bf20-9b91ec695b4c

--- widgets/ui_DiagPlots.py
+++ widgets/ui_DiagPlots.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'ui/DiagPlots.ui'
+# Form implementation generated from reading ui file 'widgets/ui/DiagPlots.ui'
 #
-# Created: Thu Jul 10 14:46:19 2014
+# Created: Thu Jul 24 11:13:00 2014
 #      by: PyQt4 UI code generator 4.9.3
 #
 # WARNING! All changes made in this file will be lost!
@@ -20,30 +20,30 @@ class Ui_Diagnostics(object):
         Diagnostics.resize(612, 820)
         self.gridLayout = QtGui.QGridLayout(Diagnostics)
         self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
-        self.topLeft = TaurusPlot(Diagnostics)
-        self.topLeft.setObjectName(_fromUtf8("topLeft"))
-        self.gridLayout.addWidget(self.topLeft, 0, 0, 1, 1)
-        self.bottomRight = TaurusPlot(Diagnostics)
-        self.bottomRight.setObjectName(_fromUtf8("bottomRight"))
-        self.gridLayout.addWidget(self.bottomRight, 3, 1, 1, 1)
-        self.middleTopLeft = TaurusPlot(Diagnostics)
-        self.middleTopLeft.setObjectName(_fromUtf8("middleTopLeft"))
-        self.gridLayout.addWidget(self.middleTopLeft, 1, 0, 1, 1)
-        self.bottomLeft = TaurusPlot(Diagnostics)
-        self.bottomLeft.setObjectName(_fromUtf8("bottomLeft"))
-        self.gridLayout.addWidget(self.bottomLeft, 3, 0, 1, 1)
-        self.middleTopRight = TaurusPlot(Diagnostics)
-        self.middleTopRight.setObjectName(_fromUtf8("middleTopRight"))
-        self.gridLayout.addWidget(self.middleTopRight, 1, 1, 1, 1)
         self.topRight = TaurusPlot(Diagnostics)
         self.topRight.setObjectName(_fromUtf8("topRight"))
         self.gridLayout.addWidget(self.topRight, 0, 1, 1, 1)
-        self.middleBottomLeft = TaurusPlot(Diagnostics)
-        self.middleBottomLeft.setObjectName(_fromUtf8("middleBottomLeft"))
-        self.gridLayout.addWidget(self.middleBottomLeft, 2, 0, 1, 1)
-        self.middleBottomRight = TaurusPlot(Diagnostics)
-        self.middleBottomRight.setObjectName(_fromUtf8("middleBottomRight"))
-        self.gridLayout.addWidget(self.middleBottomRight, 2, 1, 1, 1)
+        self.middleDownRight = TaurusPlot(Diagnostics)
+        self.middleDownRight.setObjectName(_fromUtf8("middleDownRight"))
+        self.gridLayout.addWidget(self.middleDownRight, 2, 1, 1, 1)
+        self.middleDownLeft = TaurusPlot(Diagnostics)
+        self.middleDownLeft.setObjectName(_fromUtf8("middleDownLeft"))
+        self.gridLayout.addWidget(self.middleDownLeft, 2, 0, 1, 1)
+        self.topLeft = TaurusPlot(Diagnostics)
+        self.topLeft.setObjectName(_fromUtf8("topLeft"))
+        self.gridLayout.addWidget(self.topLeft, 0, 0, 1, 1)
+        self.middleUpLeft = TaurusPlot(Diagnostics)
+        self.middleUpLeft.setObjectName(_fromUtf8("middleUpLeft"))
+        self.gridLayout.addWidget(self.middleUpLeft, 1, 0, 1, 1)
+        self.bottomLeft = TaurusPlot(Diagnostics)
+        self.bottomLeft.setObjectName(_fromUtf8("bottomLeft"))
+        self.gridLayout.addWidget(self.bottomLeft, 3, 0, 1, 1)
+        self.middleUpRight = TaurusPlot(Diagnostics)
+        self.middleUpRight.setObjectName(_fromUtf8("middleUpRight"))
+        self.gridLayout.addWidget(self.middleUpRight, 1, 1, 1, 1)
+        self.bottomRight = TaurusPlot(Diagnostics)
+        self.bottomRight.setObjectName(_fromUtf8("bottomRight"))
+        self.gridLayout.addWidget(self.bottomRight, 3, 1, 1, 1)
 
         self.retranslateUi(Diagnostics)
         QtCore.QMetaObject.connectSlotsByName(Diagnostics)
