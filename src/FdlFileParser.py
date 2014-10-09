upper case FdlSignals constants. add complement2 feature for file signals (to fix the issue with the formula signals with an IF statement). sort File menu and Tools menu elements.
git-svn-id: svn+ssh://controls01/siciliarep/svn/trunk/tango_client/ctrflyrtech_fdl/trunk@13078 a18efa20-c439-0410-bf20-9b91ec695b4c

src/FdlFileParser.py
@@ -46,7 +46,9 @@ except:#backward compatibility to pyqt 4.4.3
     from taurus.qt import Qt,QtGui,QtCore,Qwt5
 
 SEPARATOR = 0x7FFF
-LOAD_ERROR_RATE = 0.01
+LOAD_ERROR_RATE = 0.01 # as a %
+#in 128MB file there are ~67e6 samples, and this (unused) threshold 
+#means 6e3 errors in a single file.
 
 class FdlFile(FdlLogger,Qt.QObject):
     try:#normal way
@@ -128,18 +130,18 @@ class FdlFile(FdlLogger,Qt.QObject):
            FDL file (Loops|Diag).
         '''
         return SignalFields.has_key(name) and \
-               SignalFields[name].has_key(field) and \
-               SignalFields[name][field] in self._fields.keys()
+               SignalFields[name].has_key(FIELD_) and \
+               SignalFields[name][FIELD_] in self._fields.keys()
 
     def hasIandQ(self,name):
         '''Check if the given name has a pair of items in the SignalFields 
            dictionary describing I&Q to calculate an amplitude from Is&Qs 
            collected on this FDL file type (Loops|Diag).
         '''
-        hasI = SignalFields[name].has_key(I) and \
-               self._signals.has_key(SignalFields[name][I])
-        hasQ = SignalFields[name].has_key(Q) and \
-               self._signals.has_key(SignalFields[name][Q])
+        hasI = SignalFields[name].has_key(I_) and \
+               self._signals.has_key(SignalFields[name][I_])
+        hasQ = SignalFields[name].has_key(Q_) and \
+               self._signals.has_key(SignalFields[name][Q_])
         #self.debug("signal %s hasI=%s and hasQ=%s"%(name,hasI,hasQ))
         return hasI and hasQ
 
@@ -234,9 +236,12 @@ class FdlFile(FdlLogger,Qt.QObject):
         
     def processSignalSet(self):
         for keyName in self._signals.keys():
-            fieldName = SignalFields[keyName][field]
+            fieldName = SignalFields[keyName][FIELD_]
             value = self._values[self._iterator.iterindex+self._fields[fieldName]]
-            self._signals[keyName].append(float(value)/32767*1000)
+            value = float(value)/32767*1000
+            if SignalFields[keyName].has_key(TWOCOMPLEMENT_):
+                if value > 512: value -= 1024
+            self._signals[keyName].append(value)
     #--- done processing area
     ####
     
@@ -261,9 +266,9 @@ class FdlFile(FdlLogger,Qt.QObject):
         #check the signal descriptors that have an amplitude conversion
         for keyName in SignalFields.keys():
             if self.hasIandQ(keyName):
-                Itag = SignalFields[keyName][I]
+                Itag = SignalFields[keyName][I_]
                 Isignal = self._signals[Itag]
-                Qtag = SignalFields[keyName][Q]
+                Qtag = SignalFields[keyName][Q_]
                 Qsignal = self._signals[Qtag]
                 self.info("Signal %s has I (%s) and Q (%s)"%(keyName,Itag,Qtag))
                 self._signals[keyName] = np.sqrt((Isignal**2)+(Qsignal**2))

src/FdlPlotter.py
@@ -43,8 +43,6 @@ import threading
 import time
 import traceback
 
-DecimationThreshold = 100
-
 class Plotter(FdlLogger,Qt.QWidget):
     '''This class is responsible to plot in the gui a set of signals it 
        receives. The signals can be from the loops set or (exclusive) from the
@@ -103,11 +101,11 @@ class Plotter(FdlLogger,Qt.QWidget):
                 if self._isPlottableSignal(signalName):
                     self._addSignal(signalName,signalsSet[signalName])
         if len(self._loopsSignals.keys()) != 0:
-            self._loopsRanges = self.calculateRanges('loops')
+            self._loopsRanges = self.calculateMaximalRanges('loops')
             self.debug("Ranges for loops [min,max,dec] = %s"
                        %(self._loopsRanges))
         if len(self._diagSignals.keys()) != 0:
-            self._diagRanges = self.calculateRanges('diag')
+            self._diagRanges = self.calculateMaximalRanges('diag')
             self.debug("Ranges for diagnostics [min,max,dec] = %s"
                        %(self._diagRanges))
         self.prepareTimeAndDecimation()
@@ -173,7 +171,7 @@ class Plotter(FdlLogger,Qt.QWidget):
                                        self._forcePlot)
                     self._buttonSignalsDone = True
 
-    def calculateRanges(self,signalType):
+    def calculateMaximalRanges(self,signalType):
         if signalType.lower() == 'loops':
             signals = self._loopsSignals
             ranges = self._loopsRanges
@@ -293,22 +291,22 @@ class Plotter(FdlLogger,Qt.QWidget):
         #Finally check if this subdictionary has a description of the plotting 
         # location in the gui.
         return SignalFields.has_key(name) and \
-               SignalFields[name].has_key(gui) and \
-               SignalFields[name][gui].has_key(tab) and \
-               SignalFields[name][gui].has_key(plot)
+               SignalFields[name].has_key(GUI_) and \
+               SignalFields[name][GUI_].has_key(TAB_) and \
+               SignalFields[name][GUI_].has_key(PLOT_)
     
     def _isLoopsSignal(self,name):
         '''Check if the signal is plottable from the side of the loops subset.
         '''
         return self._isPlottableSignal(name) and \
-               SignalFields[name][gui][tab] in [Loops1,Loops2]
+               SignalFields[name][GUI_][TAB_] in [Loops1,Loops2]
 
     def _isDiagnosticsSignal(self,name):
         '''Check if the signal is plottable from the side of the diagnostics 
            subset.
         '''
         return self._isPlottableSignal(name) and \
-               SignalFields[name][gui][tab] in [Diag]
+               SignalFields[name][GUI_][TAB_] in [Diag]
     def getPlotWidget(self,aTabName,aPlotName):
         if hasattr(self._parent,'ui') and \
            hasattr(self._parent.ui,aTabName):
@@ -373,15 +371,16 @@ class Plotter(FdlLogger,Qt.QWidget):
             lower = ranges[0]
             upper = ranges[1]
             dec = self._globalRanges[2]
+            self.debug("%s[%d:%d:%d]"%(name,lower,upper,dec))
             y = values[lower:upper:dec]
             self.debug("linspace(%g,%g,%d)"
                        %(self._startDisplay,self._endDisplay,y.size))
             x = np.linspace(self._startDisplay,self._endDisplay,y.size)
             signal = {'title':name,'x':x,'y':y}
-            destinationTab = SignalFields[name][gui][tab]
-            destinationPlot = SignalFields[name][gui][plot]
-            plotColor = SignalFields[name][gui][color]
-            destinationAxis =  SignalFields[name][gui][axis]
+            destinationTab = SignalFields[name][GUI_][TAB_]
+            destinationPlot = SignalFields[name][GUI_][PLOT_]
+            plotColor = SignalFields[name][GUI_][COLOR_]
+            destinationAxis =  SignalFields[name][GUI_][AXIS_]
             curveProp = CurveAppearanceProperties(lColor=Qt.QColor(plotColor),
                                                   yAxis=destinationAxis)
             widget = self.getPlotWidget(destinationTab,destinationPlot)

src/FdlSignalProcessor.py
@@ -102,7 +102,7 @@ class SignalProcessor(FdlLogger,Qt.QObject):
         categories = self._splitInCategories()
         self._signalsCalculated = 0
         self._signals2Calculate = \
-                           len(categories['facade'])+len(categories['formula'])
+                           len(categories['facade'])+len(categories[FORMULA_])
         if self._doCalculations(categories, 'facade'):
             self.debug("After facade calculations %d signals ready: %s"
                        %(len(categories['ready']),categories['ready']))
@@ -110,7 +110,7 @@ class SignalProcessor(FdlLogger,Qt.QObject):
             self.error("Aborting signal processing because facade fits "\
                        "cannot be satisfied")
             return False
-        if self._doCalculations(categories, 'formula'):
+        if self._doCalculations(categories, FORMULA_):
             self.debug("After formula calculations %d signals ready: %s"
                        %(len(categories['ready']),categories['ready']))
         else:
@@ -157,7 +157,7 @@ class SignalProcessor(FdlLogger,Qt.QObject):
                      %(len(orphan),orphan))
         return {'ready':doneSignals,
                 'facade':facadeFit,
-                'formula':withFormula,
+                FORMULA_:withFormula,
                 'orphan':orphan}
     
     def _doCalculations(self,categories,inputTag):
@@ -209,10 +209,10 @@ class SignalProcessor(FdlLogger,Qt.QObject):
         return True
         
     def _takeDepenedencies(self,signalName):
-        if SignalFields[signalName].has_key(vble):
-            return [SignalFields[signalName][vble]]
-        elif SignalFields[signalName].has_key(depend):
-            return SignalFields[signalName][depend]
+        if SignalFields[signalName].has_key(VBLE_):
+            return [SignalFields[signalName][VBLE_]]
+        elif SignalFields[signalName].has_key(DEPEND_):
+            return SignalFields[signalName][DEPEND_]
         else:
             return []
 
@@ -222,18 +222,18 @@ class SignalProcessor(FdlLogger,Qt.QObject):
             self.info("Calculating linear fit on %s signal"%(signal))
             m,n = self._getFacadesMandNs(signal)
             self._signals[signal] = \
-                               (self._signals[SignalFields[signal][vble]]- n)/m
+                               (self._signals[SignalFields[signal][VBLE_]]- n)/m
         elif self._isQuadratic(signal):
             self.info("Calculating quadratic fit on %s signal"%(signal))
             c,o = self._getFacadesCandOs(signal)
             self._signals[signal] = \
-                    (self._signals[SignalFields[signal][vble]]**2/10e8/10**c)-o
+                    (self._signals[SignalFields[signal][VBLE_]]**2/10e8/10**c)-o
         elif self._isFormula(signal):
             self.info("Calculating %s using formula '%s'"
-                      %(signal,SignalFields[signal][formula]))
+                      %(signal,SignalFields[signal][FORMULA_]))
             try:
                 beamCurrent = self._getFacadesBeamCurrent()
-                self._signals[signal] = eval(SignalFields[signal][formula],
+                self._signals[signal] = eval(SignalFields[signal][FORMULA_],
                                    {'arcsin':np.arcsin,
                                     'arctan':np.arctan2,
                                     'pi':np.pi,
@@ -276,14 +276,14 @@ class SignalProcessor(FdlLogger,Qt.QObject):
     def _isFileSignal(self,signal):
         if not self._isSignalInOurSet(signal):
             return False
-        hasFieldField = SignalFields[signal].has_key(field)
-        hasI = SignalFields[signal].has_key(I)
-        hasQ = SignalFields[signal].has_key(Q)
+        hasFieldField = SignalFields[signal].has_key(FIELD_)
+        hasI = SignalFields[signal].has_key(I_)
+        hasQ = SignalFields[signal].has_key(Q_)
         return hasFieldField or (hasI and hasQ)
     def _isSignalInOurSet(self,signal):
         return signal in self._signals.keys()
     def _isVbleInOurSet(self,signal):
-        vbleName = SignalFields[signal][vble]
+        vbleName = SignalFields[signal][VBLE_]
         if self._isFileSignal(vbleName) and \
            self._isSignalInOurSet(vbleName):
             return True
@@ -292,25 +292,25 @@ class SignalProcessor(FdlLogger,Qt.QObject):
     def _isFacadeFit(self,signal):
         return self._isLinear(signal) or self._isQuadratic(signal)
     def _isLinear(self,signal):
-        if SignalFields[signal].has_key(vble) and \
-           (self._isFileSignal(SignalFields[signal][vble]) or \
-            self._isFacadeFit(SignalFields[signal][vble])):
+        if SignalFields[signal].has_key(VBLE_) and \
+           (self._isFileSignal(SignalFields[signal][VBLE_]) or \
+            self._isFacadeFit(SignalFields[signal][VBLE_])):
            #Since it has been introduced dependencies between facade vbles
-            return SignalFields[signal].has_key(slope) and \
-                   SignalFields[signal].has_key(offset)
+            return SignalFields[signal].has_key(SLOPE_) and \
+                   SignalFields[signal].has_key(OFFSET_)
         return False
     def _isQuadratic(self,signal):
-        if SignalFields[signal].has_key(vble) and \
-           (self._isFileSignal(SignalFields[signal][vble]) or \
-            self._isFacadeFit(SignalFields[signal][vble])):
+        if SignalFields[signal].has_key(VBLE_) and \
+           (self._isFileSignal(SignalFields[signal][VBLE_]) or \
+            self._isFacadeFit(SignalFields[signal][VBLE_])):
            #Since it has been introduced dependencies between facade vbles
-            return SignalFields[signal].has_key(couple) and \
-                   SignalFields[signal].has_key(offset)
+            return SignalFields[signal].has_key(COUPLE_) and \
+                   SignalFields[signal].has_key(OFFSET_)
         return False
     def _isFormula(self,signal):
-        if SignalFields[signal].has_key(formula):
+        if SignalFields[signal].has_key(FORMULA_):
             #if any of the dependencies is NOT facade fit
-            signalDependencies = copy(SignalFields[signal][depend])
+            signalDependencies = copy(SignalFields[signal][DEPEND_])
             for i,aDependency in enumerate(signalDependencies):
                 #FIXME: this is risky without a nesting control
                 if self._isFormula(aDependency) or \